14d1da733f5cdcfc11427753424ae7a5b812257d
Lucas <114770300+LucasDVG@users.noreply.github.com>
2022-10-25 11:37:15 +0100

    Add files via upload

---
+++ Hello_World.cc
@@ -0,0 +1,19 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2022-2023
+  *
+  * @file Hello_world.cc
+  * @author Lucas de Vera Gil alu0101550145@ull.edu.es
+  * @date Oct 23 2022
+  * @brief The program show on screen "Hola, mundo!" when you execute it
+  * @bug There are no known bugs
+  * @see https://jutge.org/problems/P68688
+  */
+
+#include <iostream>
+
+int main() {
+ std:: cout << "Hello world!" << std::endl;
+}
